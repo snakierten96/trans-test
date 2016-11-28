d49dcc4042ec64a84d9dec2a03d54e20d47a050d
neil.lunn <neil.lunn>
2016-11-28 14:07:55 +1100

    Add component animations Add entry animation to each route component. Include animations polyfill for older browsers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,
+  trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('routeAnimation', [
+      state('*',style({ opacity: 1, transform: 'scale(1)' })),
+      transition(':enter', [ 
+        style({ 
+          opacity: 0, 
+          transform: 'scale(.95)'
+        }), 
+        animate('1s ease-in')
+      ]),
+      transition(':leave', [
+        animate('1s ease-out', style({
+          opacity: 0,
+          transform: 'scale(.95)'
+        }))
+      ])
+    ])
+  ],
 })
 export class HomeComponent implements OnInit {
 
--- src/app/items/item-detail/item-detail.component.ts
+++ src/app/items/item-detail/item-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation,
+  trigger, state, style, transition, animate } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Observable, Subscription } from 'rxjs/Rx';
@@ -9,7 +10,25 @@ import { IItem, IItemDetail } from '../item';
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('routeAnimation', [
+      state('*',style({ opacity: 1, transform: 'scale(1)' })),
+      transition(':enter', [ 
+        style({ 
+          opacity: 0, 
+          transform: 'scale(.95)'
+        }), 
+        animate('1s ease-in')
+      ]),
+      transition(':leave', [
+        animate('1s ease-out', style({
+          opacity: 0,
+          transform: 'scale(.95)'
+        }))
+      ])
+    ])
+  ],
 })
 export class ItemDetailComponent implements OnInit, OnDestroy {
 
