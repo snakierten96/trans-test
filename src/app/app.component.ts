8b79484177e08b63ab00b84e30633d1352ffb992
neil.lunn <neil.lunn>
2016-11-30 14:38:39 +1100

    Finalize navigation styling Also add navigation items data and share above outlet as well as in sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 import { LoadingOverlayService } from './loading-overlay';
 
+interface NavItem {
+  link: string[] | string,
+  name: string,
+  icon: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'app works!';
+
+  navigation: NavItem[] = [
+    { link: ['/'], name: 'Home', icon: 'home' },
+    { link: ['/browse'], name: 'Browse', icon: 'list'  }
+  ];
 
   constructor(private titleService: Title, private _overlay: LoadingOverlayService) { }
 
